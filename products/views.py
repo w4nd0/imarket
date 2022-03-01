6ae8600a56b7b2d765c9583cdaf74adb46c8dad1
Wander Moreira <wandersantosmoreira@gmail.com>
2022-03-01 12:37:45 -0300

    feat: app products finalized

--- products/views.py
+++ products/views.py
@@ -1,4 +1,6 @@
 from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
+from utils.permissions import IsSuperUserOrReadOnly
 
 from products.serializeres import ProductSerializer
 
@@ -8,3 +10,6 @@ from .models import Product
 class ProductModelViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsSuperUserOrReadOnly]
